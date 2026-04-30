369e4e5e6b179b7d43d3d8a0654f87585a53b745
ninetails1894 <saharitesh311@gmail.com>
2026-04-30 10:39:24 +0530

    Force CORS headers manually

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,18 @@ const grievanceRoutes = require('./routes/grievances')
 
 const app = express()
 
-app.use(cors({
-  origin: function(origin, callback) {
-    callback(null, true)
-  },
-  credentials: true
-}))
+// Allow ALL origins completely
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
+app.use(cors())
 
 app.use(express.json())
 
